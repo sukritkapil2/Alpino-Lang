ed558a0c086f84f6384a6ca7127f64dda86d3db3
Sukrit Kapil <sukrit.kapil2@gmail.com>
2021-02-15 15:24:19 +0530

    Code made clean

--- driver.c
+++ driver.c
@@ -1,5 +1,5 @@
-//this c file scans through the input alpino code and seperates appropriate lexemes
-//which are then sent to the lexer that return the corresponding token
+// this c file scans through the input alpino code and seperates appropriate lexemes
+// which are then sent to the lexer that return the corresponding token
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +8,7 @@
 
 int line_number = 1;
 
-//store the reserved keywords of the language in an array
+// store the reserved keywords of the language in an array
 char* keywords[] = {"fun", "var", "return", "if", "elif", "break", "continue", "loop", "from", "to"};
 
 // returns the next character without moving the file pointer
@@ -19,7 +19,7 @@ char peek_next_char(FILE* fptr) {
     return c;
 }
 
-// increments the file pointer by one to the right
+// increments the file pointer by one to the right, also takes care of line number
 void move_pointer_next(FILE* fptr) {
     char c = fgetc(fptr);
     if(c == '\n') line_number++;
@@ -30,50 +30,52 @@ void move_pointer_back(FILE* fptr) {
     fseek(fptr, -1, SEEK_CUR);
 }
 
-//skips till the c_next is a non whitespace character
+// moves the pointer to the last white space or to the last valid character
 void skip_white_spaces(FILE* fptr) {
     char c_next = peek_next_char(fptr);
+    if(c_next != ' ' && c_next != '\n') return;
     while(c_next == ' ' || c_next == '\n') {
+        if(c_next == '\n') line_number++;
         move_pointer_next(fptr);
         c_next = peek_next_char(fptr);
     }
 }
 
-void lexer_assignment_found(char c, FILE* fptr) {
-    printf("TK-assignment, string =, line number %d\n", line_number);
 
+// after all the methods, the file pointer should point to the last valid character of the lexeme found
+
+void lexer_assignment_found(char c, FILE* fptr) {
     char c_next = peek_next_char(fptr);
-    while(c_next != ' ' && c_next != '\n') {
+    if(c_next == '=') {
+        printf("TK-operator, string ==, line number %d\n", line_number);
         move_pointer_next(fptr);
-        c_next = peek_next_char(fptr);
     }
-    move_pointer_next(fptr);
+    else printf("TK-assignment, string =, line number %d\n", line_number);
+
+    skip_white_spaces(fptr);
 
     c_next = peek_next_char(fptr);
-    //printf("Next char: %c\n", c_next);
 
-    //checks if the next character is an identifier
+    // checks if the next character is an identifier
     if((c_next >= 65 && c_next <= 90) || (c_next >= 97 && c_next <= 122) || c_next == '_') {
         return;
     } else if (c_next == '+' || c_next == '-' || (c_next >= 48 && c_next <= 57)) {
-        //the lexeme is an integer literal
+        // the lexeme is an integer literal
         char str[] = "";
+
         if(c_next == '+' || c_next == '-') {
             strncat(str, &c_next, 1);
             move_pointer_next(fptr);
-            c_next = peek_next_char(fptr);
-            while(c_next == ' ' || c_next == '\n') {
-                move_pointer_next(fptr);
-                c_next = peek_next_char(fptr);
-            }
+            skip_white_spaces(fptr);
         }
+
+        c_next = peek_next_char(fptr);
 
         while(c_next >= 48 && c_next <= 57) {
             strncat(str, &c_next, 1);
             move_pointer_next(fptr);
             c_next = peek_next_char(fptr);
         }
-        move_pointer_next(fptr);
 
         char* s_copy;
         s_copy = (char*)malloc(100*sizeof(char));
@@ -94,20 +96,18 @@ void lexer_assignment_found(char c, FILE* fptr) {
     }
 }
 
-char* lexer_alphabet_found(char c, FILE* fptr) {
+void lexer_alphabet_found(char c, FILE* fptr) {
     // the lexeme can either be a keyword or an identifier
     
-    //s_temp is the word character array
+    // s_temp is the word character array which will store the lexeme
     char s_temp[] = "";
     char c_next = c;
-    int size = 0;
 
+    // move the pointer forward till we see an alphabet, a digit or and underscore
     while((c_next >= 65 && c_next <= 90) || (c_next >= 97 && c_next <= 122) || (c_next >= 48 && c_next <= 57) || c_next == '_') {
         strncat(s_temp, &c_next, 1);
-        //printf("%c\n", c_next);
         c_next = peek_next_char(fptr);
         move_pointer_next(fptr);
-        size++;
     }
     move_pointer_back(fptr);
 
@@ -116,7 +116,7 @@ char* lexer_alphabet_found(char c, FILE* fptr) {
     s_copy = (char*)malloc(100*sizeof(char));
     strcpy(s_copy, s_temp);
 
-    //search for the word in the keywords array
+    // search for the word in the keywords array
     for(int i = 0;i < KEYWORDS_SIZE; i++) {
         if(strcmp(keywords[i], s_temp) == 0) {
             char str[100] = "TK-keyword, string ";
@@ -126,10 +126,12 @@ char* lexer_alphabet_found(char c, FILE* fptr) {
             s = (char*)malloc(100*sizeof(char));
             strcpy(s, str);
 
-            return s;
+            printf("%s, line number %d\n", s, line_number);
+            return;
         }
     }
 
+    // if the lexeme is not a keyword
     char str[100] = "TK-identifier, string ";
     strcat(str, s_copy);
 
@@ -137,7 +139,8 @@ char* lexer_alphabet_found(char c, FILE* fptr) {
     s = (char*)malloc(200*sizeof(char));
     strcpy(s, str);
 
-    return s;
+    printf("%s, line number %d\n", s, line_number);
+    return;
 }
 
 void lexer_operator_found(char c, FILE* fptr) {
@@ -149,7 +152,8 @@ void lexer_operator_found(char c, FILE* fptr) {
         case '/':
             if(c_next == '/') {
                 move_pointer_next(fptr);
-                //to ignore the comment statement, move the file pointer forward till the next line or the end of file
+                c_next = peek_next_char(fptr);
+                // to ignore the comment statement, move the file pointer forward till the next line or the end of file
                 while(c_next != '\n' && c_next != EOF) {
                     move_pointer_next(fptr);
                     c_next = peek_next_char(fptr);
@@ -167,7 +171,8 @@ void lexer_operator_found(char c, FILE* fptr) {
                 printf("TK-operator, string +, line number %d\n", line_number);
                 skip_white_spaces(fptr);
 
-                //we have an integer literal
+                // we have an integer literal in arithmetic operation
+                c_next = peek_next_char(fptr);
                 if(c_next >= 48 && c_next <= 57) {
                     char str[] = "";
                     
@@ -187,7 +192,25 @@ void lexer_operator_found(char c, FILE* fptr) {
                 move_pointer_next(fptr);
                 printf("TK-operator, string --, line number %d\n", line_number);
                 return;
-            } else break;
+            } else {
+                printf("TK-operator, string -, line number %d\n", line_number);
+                skip_white_spaces(fptr);
+
+                // we have an integer literal in arithmetic operation
+                c_next = peek_next_char(fptr);
+                if(c_next >= 48 && c_next <= 57) {
+                    char str[] = "";
+                    
+                    while(c_next >= 48 && c_next <= 57) {
+                        strncat(str, &c_next, 1);
+                        move_pointer_next(fptr);
+                        c_next = peek_next_char(fptr);
+                    }
+
+                    printf("TK-integer, string %s, line number %d\n", str, line_number);
+                    return;
+                }
+            }
 
         case ':':
             if(c_next == '=') {
@@ -245,38 +268,37 @@ void lexer_operator_found(char c, FILE* fptr) {
 }
 
 int main() {
-    //pointer to a file
+    // pointer to a file
     char c;
     FILE* fptr;
 
+    // return error if file cannot be opened
     if((fptr = fopen("./input.al", "r")) == NULL) {
         printf("Error opening the file!\n");
         exit(1);
     }
 
-    //c is the character in the file
+    // peek the next character, without moving the file pointer
     c = peek_next_char(fptr);
     
-    //loop through all the characters in the file
+    // loop through all the characters in the file
     while(c != EOF) {
-        //printf("%c\n", c);
-        //printf("%c", c);
-        //if(c == '\n') line_number++;
+        
+        // move the pointer to the current character, so now we are pointing to the current character
         move_pointer_next(fptr);
         
         if(c == ' ' || c == '\n') {
             
         }
-        //checks if the lexeme is a keyword or an identifier
+        // checks if the lexeme is a keyword or an identifier
         else if((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == '_') {
-            char* str = lexer_alphabet_found(c, fptr);
-            printf("%s, line number %d\n", str, line_number);
+            lexer_alphabet_found(c, fptr);
         }
-        //checks if the lexeme is an operator or a comment
+        // checks if the lexeme is an operator or a comment
         else if(c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '>' || c == '<' || c == '!' || c == '?' || c == ':' || c == '&' || c == '|') {
             lexer_operator_found(c, fptr);
         }
-        //checks if the lexeme is an assignment operator
+        // checks if the lexeme is an assignment operator
         else if(c == '=') {
             lexer_assignment_found(c, fptr);
         }
@@ -285,6 +307,7 @@ int main() {
         c = peek_next_char(fptr);
     }
 
+    // close the file
     fclose(fptr);
 
     return 0;
